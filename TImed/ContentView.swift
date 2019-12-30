320af19d0458e818eb61eb9867f64625d0fd49ee
Matthew Mcavey <34047196+Allazzanni@users.noreply.github.com>
2019-12-30 12:35:04 -0500

    Initial Commit

---
+++ TImed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TImed
+//
+//  Created by Matthew McAvey on 12/30/19.
+//  Copyright © 2019 Matthew McAvey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
